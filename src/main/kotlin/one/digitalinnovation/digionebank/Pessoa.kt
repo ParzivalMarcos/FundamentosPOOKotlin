7bb39f63a70c88278dfc4f28de612e87ff099dc6
Marcos Lima Marinho <marcolim977@gmail.com>
2021-06-29 22:28:04 -0300

    Add inner class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,6 +3,10 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Marcos"
     var cpf: String = "111.222.333-44"
+
+    inner class Endereco {
+        var rua: String = "Rua ABC"
+    }
 }
 
 fun main() {
@@ -10,4 +14,5 @@ fun main() {
 
     println(marcos.nome)
     println(marcos.cpf)
+    println(marcos.Endereco().rua)
 }
