2ee3db581b3252e3d36b807c0f91f067cd4bf73f
Marcos Lima Marinho <marcolim977@gmail.com>
2021-06-29 22:15:30 -0300

    Created person class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,13 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Marcos"
+    var cpf: String = "111.222.333-44"
+}
+
+fun main() {
+    val marcos = Pessoa()
+
+    println(marcos.nome)
+    println(marcos.cpf)
 }
