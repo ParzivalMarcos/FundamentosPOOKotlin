fa7e5219ed3eaae3de5b36df8105f715fc84bc53
Marcos Lima Marinho <marcolim977@gmail.com>
2021-07-01 22:29:13 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,22 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Marcos"
+abstract class Pessoa(
+    var nome: String,
+    var cpf: String
+)
 
-    var cpf: String = "111.222.333-44"
-    private set // Impede que seja os valores sejam alterados
-
-    constructor()
-
-    fun pessoaInfo() = "Nome: $nome \nCPF: $cpf"
-
-    inner class Endereco {
-        var rua: String = "Rua ABC"
-    }
-}
-
-fun main() {
-    val marcos = Pessoa()
-
-    println(marcos.pessoaInfo())
-}
