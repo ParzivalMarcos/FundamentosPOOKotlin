3979e72a8af34b345b9f0bef0a3914686e6bfac6
Marcos Lima Marinho <marcolim977@gmail.com>
2021-07-02 00:05:27 -0300

    updated manage test

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteGerente.kt
@@ -3,7 +3,9 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val marcos = Gerente(nome = "Marcos", cpf = "111.222.333-44", salario = 10000.0)
+    val marcos = Gerente(nome = "Marcos", cpf = "111.222.333-44", salario = 10000.0, senha = "1q2w3e")
 
     ImprimeRelatorioFuncionario.imprime(marcos)
+
+    TesteAutenticacao().autentica(marcos)
 }
